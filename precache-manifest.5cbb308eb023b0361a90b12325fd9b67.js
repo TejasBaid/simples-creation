d889ab750f5847686399898f86099b99e538b6fd
tejasbaid <tejasbaid3@gmail.com>
2020-09-01 18:12:23 +0530

    Updates

---
+++ precache-manifest.5cbb308eb023b0361a90b12325fd9b67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5834dd9f0587113052a7f552ac6f737b",
+    "url": "/simples-creation/index.html"
+  },
+  {
+    "revision": "d5f5eed03b6ada99b925",
+    "url": "/simples-creation/static/css/main.be98865f.chunk.css"
+  },
+  {
+    "revision": "6c53e49b5b7003cd5a7c",
+    "url": "/simples-creation/static/js/2.94a85c43.chunk.js"
+  },
+  {
+    "revision": "9a8a64d30a81695e7691dd446a703cba",
+    "url": "/simples-creation/static/js/2.94a85c43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5f5eed03b6ada99b925",
+    "url": "/simples-creation/static/js/main.20b27973.chunk.js"
+  },
+  {
+    "revision": "4f7d1b81c92d317a7b04",
+    "url": "/simples-creation/static/js/runtime-main.f401fc2d.js"
+  }
+]);
